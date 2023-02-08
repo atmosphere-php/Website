911602a110cec37d344e56b4b06ae6c7fa00742f
Romano Schoonheim <romano@consumind.nl>
2023-02-08 15:07:31 +0100

    Update entrypoints and framework.

--- Entrypoints/Console.php
+++ Entrypoints/Console.php
@@ -2,5 +2,11 @@
 
 declare(strict_types=1);
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
+use Atmospherephp\Framework\Console\ConsoleKernel;
+use Atmospherephp\Framework\Foundation\Application;
+
+$application = new Application(
+    kernel: new ConsoleKernel(),
+);
--- Entrypoints/Http.php
+++ Entrypoints/Http.php
@@ -2,7 +2,13 @@
 
 declare(strict_types=1);
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
+use Atmospherephp\Framework\Http\HttpKernel;
+use Atmospherephp\Framework\Foundation\Application;
+
+$application = new Application(
+    kernel: new HttpKernel(),
+);
 
 echo "Sooner or later, I'll be a web application.";
